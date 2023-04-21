6fc83fdb453bf0becccd2a283afb9a89842cd84c
norasiv <norasiv@hotmail.com>
2023-04-21 22:07:00 +0200

    Initial Commit

---
+++ ShortcutChallenge/ComicModel.swift
@@ -0,0 +1,8 @@
+//
+//  ComicModel.swift
+//  ShortcutChallenge
+//
+//  Created by Nora Sivertsen Bull on 21/04/2023.
+//
+
+import Foundation
---
+++ ShortcutChallenge/ComicViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShortcutChallenge
+//
+//  Created by Nora Sivertsen Bull on 21/04/2023.
+//
+
+import UIKit
+
+class ComicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
