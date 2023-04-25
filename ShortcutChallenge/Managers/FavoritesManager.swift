e9e6e3c026a65d1731bed8b0af3867f0c2aa2dbf
norasiv <norasiv@hotmail.com>
2023-04-25 22:58:25 +0200

    set up folders

---
+++ ShortcutChallenge/Managers/FavoritesManager.swift
@@ -0,0 +1,8 @@
+//
+//  FavoritesManager.swift
+//  ShortcutChallenge
+//
+//  Created by Nora Sivertsen Bull on 25/04/2023.
+//
+
+import Foundation
---
+++ ShortcutChallenge/ViewControllers/FavoritesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesViewController.swift
+//  ShortcutChallenge
+//
+//  Created by Nora Sivertsen Bull on 25/04/2023.
+//
+
+import Foundation
+import UIKit
+
+
+class FavoritesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
