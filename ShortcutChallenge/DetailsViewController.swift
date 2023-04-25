d436fc57a9c396aa763127d8c638f3ea250a17f4
norasiv <norasiv@hotmail.com>
2023-04-25 20:30:14 +0200

    Details showing in DetailsViewController

--- ShortcutChallenge/DetailsViewController.swift
+++ ShortcutChallenge/DetailsViewController.swift
@@ -8,7 +8,20 @@
 import Foundation
 import UIKit
 
-class DetailsViewController: UICollectionViewController {
+class DetailsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var numLabel: UILabel!
+    @IBOutlet weak var linkLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var newsLabel: UILabel!
+    @IBOutlet weak var safeLabel: UILabel!
+    @IBOutlet weak var transcriptLabel: UILabel!
+    @IBOutlet weak var altLabel: UILabel!
+    @IBOutlet weak var imgLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
     
     var comicManager = ComicManager()
     var comicId = 0
@@ -16,9 +29,7 @@ class DetailsViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Details"
         comicManager.delegate = self
-        
         fetchedComic()
     }
     
@@ -35,7 +46,17 @@ class DetailsViewController: UICollectionViewController {
 extension DetailsViewController: ComicManagerDelegate {
     func didFetchComic(_ comicManager: ComicManager, comic: Comic) {
         DispatchQueue.main.async {
-            print(comic.title)
+            self.monthLabel.text = "Month: \(comic.month)"
+            self.numLabel.text = "Num: \(String(comic.num))"
+            self.linkLabel.text = "Link: \(comic.link)"
+            self.yearLabel.text = "Year: \(comic.year)"
+            self.newsLabel.text = "News: \(comic.news)"
+            self.safeLabel.text = "Safe Title: \(comic.safe_title)"
+            self.transcriptLabel.text = "Transcript: \(comic.transcript)"
+            self.altLabel.text = "Alt: \(comic.alt)"
+            self.imgLabel.text = "Img: \(comic.img)"
+            self.titleLabel.text = "Title: \(comic.title)"
+            self.dayLabel.text = "Day: \(comic.day)"
         }
     }
     
--- ShortcutChallenge/Helpers/Alert.swift
+++ ShortcutChallenge/Helpers/Alert.swift
@@ -23,13 +23,13 @@ extension UIViewController {
 
 //MARK: - Spinner
 var spinner : UIView?
-let indicator = UIActivityIndicatorView(frame: CGRect(x: 150, y: 180, width: 30, height: 30))
+let indicator = UIActivityIndicatorView(frame: CGRect(x: 170, y: 160, width: 30, height: 30))
 
 extension UIViewController {
     
     func spinnerAlert(onView : UIView) {
         let spinnerView = UIView.init(frame: onView.bounds)
-        spinnerView.frame = CGRect(x: 33, y: 300, width: 350, height: 360)
+        spinnerView.frame = CGRect(x: 10, y: 300, width: 370, height: 360)
         spinnerView.backgroundColor = UIColor.white
         
         indicator.hidesWhenStopped = true
