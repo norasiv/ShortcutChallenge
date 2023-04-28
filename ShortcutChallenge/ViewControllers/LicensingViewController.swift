01e7ff90ab9def3df0b75182e18cd65a8865058f
norasiv <norasiv@hotmail.com>
2023-04-28 16:33:52 +0200

    Licensing info

---
+++ ShortcutChallenge/ViewControllers/LicensingViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LicensingViewController.swift
+//  ShortcutChallenge
+//
+//  Created by Nora Sivertsen Bull on 28/04/2023.
+//
+
+import Foundation
+import UIKit
+
+
+class LicensingViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var licenseLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Licensing"
+        
+        licenseLabel.text = "This work is licensed under a Creative Commons Attribution-NonCommercial 2.5 License"
+    }
+    
+    
+    @IBAction func goToPressed(_ sender: Any) {
+        guard let url = URL(string: "https://xkcd.com") else { return }
+        UIApplication.shared.open(url)
+    }
+    
+}
